0717f37c36418e260eda97a55a9cba1dd1b81afe
23harrellan04 <124181361+23harrellan04@users.noreply.github.com>
2023-02-14 10:33:00 -0500

    Add files via upload

---
+++ my_mod.py
@@ -0,0 +1,10 @@
+def my_function():
+    print("I am a function yaaaaaaay")
+
+def make_list():
+    return[1,2,3]
+
+if __name__ == "__main__":
+    print("this is me running the main file")
+else:
+    print("this is me running from being imported")
